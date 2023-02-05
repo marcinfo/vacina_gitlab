d8891ac7d3d2c68064881864f405e2a086dff09a
marcinfo <marc_info@yahoo.com.br>
2023-02-05 11:49:01 -0300

    refinalisando

--- vacina/models.py
+++ vacina/models.py
@@ -21,8 +21,6 @@ class Profile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     date_of_birth = models.DateField(blank=True, null=True, verbose_name='Data de Nascimento')
     photo = models.ImageField(upload_to='users/%Y/%m/%d/', blank=True, verbose_name='Foto')
-
-
     def __str__(self):
         return f'Profile for user {self.user.username}'
 
