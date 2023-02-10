37f53686dd9865dfb32869cc93f7dbf92bc5808c
marcinfo <90268921+marcinfo@users.noreply.github.com>
2023-02-10 13:38:40 -0300

    customizndo API

--- vacina/api.py
+++ vacina/api.py
@@ -1,3 +1,4 @@
+import pandas as pd
 from ninja import  NinjaAPI
 from .models import  TbCalendarioVacina
 
@@ -5,10 +6,17 @@ import json
 apis = NinjaAPI( title = 'API Vacina Paulistana' )
 @apis.get('prazos/')
 def listar(request):
-    prazos = TbCalendarioVacina.objects.all()
+    prazos = TbCalendarioVacina.objects.filter().order_by('-descricao_vacina').reverse()
     response = [{ 'vacina': i.descricao_vacina,\
                  'observacao': i.observacao,'meses': i.meses} for i in prazos]
-    print(response)
+
 
     return response
+
+@apis.get('vacinas/')
+def listar(request):
+    vacinas = TbCalendarioVacina.objects.values('descricao_vacina','observacao').order_by('descricao_vacina').distinct()
+    vacinas_lista = list(vacinas)
+    return vacinas_lista
+
 
