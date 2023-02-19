4adaf8990ff5745efcca41b9978730b221fbf219
maosantos <M@rcela20>
2023-02-19 14:19:01 -0300

    Calculando e exibindo as 10 ubs mais proximas. testado com ubs fora da cidade (simulada).

--- vacinapaulistana/settings.py
+++ vacinapaulistana/settings.py
@@ -10,7 +10,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '@^k78$e&p+sm$!9^u+e&!mnd!ercjq4j-t-k!0$r7-r#i4mf#u'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
 ALLOWED_HOSTS = ['*']
 
 
